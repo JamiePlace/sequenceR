059520339d4fed67a1e5967711da6cd007ba4db3
Jamie Place <jamieplace95@gmail.com>
2021-03-22 19:16:36 +0000

    improved efficiency

--- R/accessibility.R
+++ R/accessibility.R
@@ -94,9 +94,9 @@ find_sequences <- function(data, window = 1, continuous = TRUE, required_pad = 1
 
     # pad the result with 0
     x1 <- torch::nnf_pad(input = x1, pad = c(0, x$shape[3] - x1$shape[3]), mode = 'constant', value = 0)
-    x1 <- x1$squeeze()
-    x1 <- x1$add(0.0001)
-    x1 <- x1$floor()
+    x1$squeeze_()
+    x1$add_(0.0001)
+    x1$floor_()
 
     accessible_times[start, ] <- x1
   }
