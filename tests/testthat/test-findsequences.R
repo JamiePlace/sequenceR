17b99e48984f5a99645e791b3f1bed4ee4b79752
Jamie Place <jamieplace95@gmail.com>
2021-04-02 20:28:25 +0100

    added test yaml

--- tests/testthat/test-findsequences.R
+++ tests/testthat/test-findsequences.R
@@ -13,3 +13,35 @@ test_that("sequences can be found non-continuous", {
 
   expect_equal(y, c(0,0,1,0,1,0,1,0,0))
 })
+
+test_that("long sequences can be found continuous", {
+
+  wind <- 1235
+
+  x <- rep(1, wind)
+  x <- c(rep(0, wind*3), x)
+
+  y <- find_sequences(x, window = wind, continuous = TRUE)
+
+  y_ <- rep(0,length(x))
+
+  min_x <- min(which(x==1))
+  y_[min_x] <- 1
+
+  expect_equal(y, y_)
+})
+
+test_that("large windows are accurate", {
+
+  wind <- 1235
+
+  x <- rep(1, wind)
+  x <- c(rep(0, wind*3), x)
+  x[length(x)] <- 0
+
+  y <- find_sequences(x, window = wind, continuous = TRUE)
+
+  y_ <- rep(0,length(x))
+
+  expect_equal(y, y_)
+})
